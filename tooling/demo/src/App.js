478ecb964ce270e1d5a2e9c261766541fa1482a3
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-02-12 15:06:41 +0100

    Tooling

--- tooling/demo/src/App.js
+++ tooling/demo/src/App.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Hello from "./Hello";
 
 class App extends Component {
+
+getStuff()
+{
+  return 
+  {
+    x:1
+  }
+}
   render() {
+
+
+    var x = 1;
+    if (x === '1'){
+
+    }
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Hello name="world"/>
       </div>
     );
   }
---
+++ tooling/demo/src/Hello.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+interface HelloProps {
+    name: string
+}
+
+class Hello extends React.Component<HelloProps, {}>{
+    render() {
+        const {name} = this.props;
+
+        return <div>Hello {name}</div>
+    }
+}
+
+export default Hello;
