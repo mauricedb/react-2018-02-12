2fe00d5367ee8869947a279a5b393470e352ef10
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-02-12 19:17:22 +0100

    Support redux-devtools

--- Redux/redux-demo/src/redux/createAppStore.js
+++ Redux/redux-demo/src/redux/createAppStore.js
@@ -1,12 +1,14 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import moviesReducer from './moviesReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () =>
   createStore(
     combineReducers({
       movies: moviesReducer
     }),
-    applyMiddleware(thunk)
+    composeEnhancers(applyMiddleware(thunk))
   );
