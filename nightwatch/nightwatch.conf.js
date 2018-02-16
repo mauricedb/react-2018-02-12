e732da970626fbbe01a54d9c6490496ad03d6ab0
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-02-16 11:30:43 +0100

    Using Edge, FireFox and Chrome

---
+++ nightwatch/nightwatch.conf.js
@@ -0,0 +1,24 @@
+const seleniumServer = require('selenium-server');
+const chromedriver = require('chromedriver');
+
+module.exports = {
+  src_folders: ['tests'],
+  selenium: {
+    start_process: true,
+    server_path: seleniumServer.path,
+    cli_args: {
+      'webdriver.chrome.driver': chromedriver.path,
+      'webdriver.gecko.driver': './geckodriver-v0.19.1-win64/geckodriver.exe',
+      'webdriver.edge.driver': './MicrosoftWebDriver.exe'
+    }
+  },
+  test_settings: {
+    default: {
+      selenium_port: 4444,
+      selenium_host: 'localhost',
+      desiredCapabilities: {
+        browserName: 'MicrosoftEdge'
+      }
+    }
+  }
+};
