ec24d4e6a69ea8a981021c5a90775cdf11b78abd
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-02-17 09:05:46 +0100

    Using NPM geckodriver package

--- nightwatch/nightwatch.conf.js
+++ nightwatch/nightwatch.conf.js
@@ -1,5 +1,6 @@
 const seleniumServer = require('selenium-server');
 const chromedriver = require('chromedriver');
+const geckodriver = require('geckodriver');
 
 module.exports = {
   src_folders: ['tests'],
@@ -8,21 +9,32 @@ module.exports = {
     server_path: seleniumServer.path,
     cli_args: {
       'webdriver.chrome.driver': chromedriver.path,
-      'webdriver.gecko.driver': './geckodriver-v0.19.1-win64/geckodriver.exe',
+      'webdriver.gecko.driver': geckodriver.path,
       'webdriver.edge.driver': './MicrosoftWebDriver.exe'
     }
   },
   test_settings: {
     default: {
+      screenshots: {
+        enabled: true,
+        on_failure: true,
+        on_error: true,
+        path: './screenshots'
+      },
       selenium_port: 4444,
       selenium_host: 'localhost',
       desiredCapabilities: {
         browserName: 'chrome'
       }
     },
-    firefox : {
+    firefox: {
       desiredCapabilities: {
-        browserName: "firefox"
+        browserName: 'firefox'
+      }
+    },
+    edge: {
+      desiredCapabilities: {
+        browserName: 'MicrosoftEdge'
       }
     }
   }
